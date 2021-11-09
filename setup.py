7a9d100b8ef400601a66eb430874179cbec69bac
zhoufan <zhoufan@cdsslh.com>
2021-11-09 13:13:43 +0000

    [问题/需求描述]:补全依赖 [解决方法]:补全依赖 [影响范围]:无 [需求编号]:无
    Change-Id: I1a209f1a929c18d84c016ea7691cbae6bebf2e11

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ setup(
         'numpy>=1.19.1',
         'data_generator>=1.4',
         'pytz>=2021.1',
-        'statsmodels>=0.12.2'
+        'statsmodels>=0.12.2',
+        'mplfinance>=0.12.7a17'
     ],
 
     # 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
