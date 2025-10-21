3793910729d506a825963cd4e739b4500d0ca5da
zhoufan <zhoufan@tsaodai.com>
2025-10-21 09:20:51 +0800

    [问题/需求描述]:升级版本到2.6.8 [解决方法]:升级版本到2.6.8 [影响范围]:无 [需求编号]:无
    Change-Id: Iebd5265f9f4e5543896ef28f680d282048327029

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst', 'r', encoding='utf-8') as f:
 
 setup(
     name="ticknature",
-    version="2.6.7",
+    version="2.6.8",
     author="zhoufan",
     author_email="zhoufan@tsaodai.com",
     description="Nature layer",
